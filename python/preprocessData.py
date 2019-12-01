66b0016ab235e7bb54e1d9e79fa86187b33fb5d6
Neil Schroeder <neil_schroeder@ymail.com>
2019-12-01 16:34:16 -0600

    preprocess data and do some simply ml on 1 stock

--- python/preprocessData.py
+++ python/preprocessData.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import pickle
+from collections import Counter
+from sklearn import svm, neighbors, model_selection
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+
 
 def processDataForLabels(ticker):
     hm_days = 7
@@ -16,7 +20,7 @@ def processDataForLabels(ticker):
 
 def buySellHold(*args):
     cols = [ c for c in args ]
-    requirement = 0.2
+    requirement = 0.025
     for col in cols:
         if col > requirement:
             return 1
@@ -27,14 +31,14 @@ def buySellHold(*args):
 def extractFeatureSet(ticker):
     tickers, df = processDataForLabels(ticker)
 
-    df['{}_tarte'.format(ticker)] = list(map(buySellHold,
-                                              df['{}_1d'.format(ticker, i)],
-                                              df['{}_2d'.format(ticker, i)],
-                                              df['{}_3d'.format(ticker, i)],
-                                              df['{}_4d'.format(ticker, i)],
-                                              df['{}_5d'.format(ticker, i)],
-                                              df['{}_6d'.format(ticker, i)],
-                                              df['{}_7d'.format(ticker, i))
+    df['{}_target'.format(ticker)] = list(map(buySellHold,
+                                              df['{}_1d'.format(ticker)],
+                                              df['{}_2d'.format(ticker)],
+                                              df['{}_3d'.format(ticker)],
+                                              df['{}_4d'.format(ticker)],
+                                              df['{}_5d'.format(ticker)],
+                                              df['{}_6d'.format(ticker)],
+                                              df['{}_7d'.format(ticker)]))
 
     vals = df['{}_target'.format(ticker)].values.tolist()
     str_vals = [str(i) for i in vals]
@@ -44,4 +48,34 @@ def extractFeatureSet(ticker):
     df = df.replace([np.inf, -np.inf], np.nan)
     df.dropna(inplace=True)
 
-    df_vals = 
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    x = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return x,y,df
+
+def machineLearn(ticker):
+    x, y, df = extractFeatureSet(ticker)
+
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x,
+                y,
+                test_size = 0.2)
+
+    #clf = neighbors.KNeighborsClassifier()
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+
+    clf.fit(x_train, y_train)
+    confidence = clf.score(x_test, y_test)
+    print('Accuracy', confidence)
+    predictions = clf.predict(x_test)
+
+    print('Predicted spread:', Counter(predictions))
+
+    return confidence
+
+machineLearn('MMM')
